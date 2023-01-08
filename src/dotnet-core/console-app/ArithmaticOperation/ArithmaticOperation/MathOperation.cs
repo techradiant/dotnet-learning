312471df994c86752f1578dddbf763e779b55e8c
Prakhar Sharma <sharma.prakhar1@outlook.com>
2023-01-08 19:20:32 +0530

    creating arithmetic operations

---
+++ src/dotnet-core/console-app/ArithmaticOperation/ArithmaticOperation/MathOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmaticOperation
+{
+    internal class MathOperation
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+        public int Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+
+}
---
+++ src/dotnet-core/console-app/ArithmaticOperation/ArithmaticOperation/Program.cs
@@ -0,0 +1,33 @@
+﻿using ArithmaticOperation;
+using System;
+
+namespace ArithmaticOperations // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int option = -1;
+            MathOperation mathOp = new MathOperation();
+            Console.WriteLine("Select Option");
+            Console.WriteLine("1.Add");
+            Console.WriteLine("2.Subtract");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            Console.Write("Enter your option:");
+            string optionText = Console.ReadLine();
+            Console.WriteLine("Enter First Number:"); 
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+            int result = 0;
+            if (optionText == "1") 
+            {
+                result = mathOp.Add(firstNumber, secondNumber);
+            }
+            Console.WriteLine($"Result={result}");
+        }
+        
+    }
+}
