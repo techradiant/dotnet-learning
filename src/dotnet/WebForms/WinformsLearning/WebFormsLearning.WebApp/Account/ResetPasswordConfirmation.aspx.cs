48ea270616d044ca5b1390b9cdb9092a88475712
Niranjan Singh <niranjan.kala@gmail.com>
2023-08-01 20:06:20 +0530

    Added web forms learning project

---
+++ src/dotnet/WebForms/WinformsLearning/WebFormsLearning.WebApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebFormsLearning.WebApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ src/dotnet/WebForms/WinformsLearning/WebFormsLearning.WebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsLearning.WebApp.Startup))]
+namespace WebFormsLearning.WebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
