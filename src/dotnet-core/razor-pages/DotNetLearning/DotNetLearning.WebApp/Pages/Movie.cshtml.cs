61a2c7dc1b5aca449039ae05b2b4dbc1db24f321
Niranjan Singh <niranjan.kala@gmail.com>
2023-08-23 12:28:31 +0530

    Getting data by ID from a database

--- src/dotnet-core/razor-pages/DotNetLearning/DotNetLearning.WebApp/Pages/Movie.cshtml.cs
+++ src/dotnet-core/razor-pages/DotNetLearning/DotNetLearning.WebApp/Pages/Movie.cshtml.cs
@@ -1,12 +1,23 @@
+using DotNetLearning.WebApp.Data;
+using DotNetLearning.WebApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace DotNetLearning.WebApp.Pages
 {
     public class MovieModel : PageModel
     {
-        public void OnGet()
+        public Movie Movie { get; set; }
+
+        private ApplicationDbContext _context;
+        public MovieModel(ApplicationDbContext context)
         {
+                _context = context;
+        }
+        public void OnGet(int id)
+        {
+            Movie = _context.Movies.FirstOrDefault(m => m.Id == id);
         }
     }
 }
